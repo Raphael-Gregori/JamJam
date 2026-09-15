29a176d7edcc04f515e1cd7b36da9f6570a05996
Raphael G. <raphael.gregori.gd@gmail.com>
2026-09-15 12:32:54 +0200

    add global stats TODO : Need to wire it all to P1 and P2 current stats with a getter setter

---
+++ jamjam_godot_project/nouveau-projet-de-jeu/global_stats.gd
@@ -0,0 +1,22 @@
+extends Node
+class_name Global_stats
+
+
+@export var _CONST_DAMAGE = 1.0
+@export var _CONST_DEFENSE_ZONE = 0.3
+@export var _CONST_PA_GAUGE = 2.0
+
+
+@export var _CONST_PA_REGEN = 1.0
+
+@export var _CONST_SPEED_ACTIONS = 400
+
+
+@export var _CONST_DODGE_RANGE = 52.0
+
+
+@export var _CONST_DODGE_RANGE_GAUGE = 1.0
+
+
+@export var _CONST_DODGE_BASE_REGEN = 0.4
+
